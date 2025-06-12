b2c969a6deed7fba72e64d435b236798c06b87ec
Your Name <you@example.com>
2025-06-12 12:32:37 +0600

    backup

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ terraform {
       version = "~> 3.0.2"
     }
   }
-
   required_version = ">= 1.1.0"
 }
 
@@ -18,9 +17,49 @@ resource "azurerm_resource_group" "rg" {
   location = "Central India"
 }
 
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = "portfolio-prod-centralindia-sampleapi-VNET"
+  address_prefixes     = ["237.84.2.178/24"]
+}
+resource "azurerm_network_interface" "nic" {
+  name                = "portfolio-prod-centralindia-sampleapi-NIC"
+  location            = "Central India"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 
 resource "azurerm_virtual_machine" "vm" {
   name = "portfolio-prod-centralindia-sampleapi"
   location = "Central India"
+  vm_size = "Standard_D2s_v3"
   resource_group_name = azurerm_resource_group.rg.name
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  os_profile {
+    computer_name  = "myvm"
+    admin_username = "azureuser"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }

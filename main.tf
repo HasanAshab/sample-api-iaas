c62f0d5b649c0fdf220e16025400a33b8e3d0dfd
Your Name <you@example.com>
2025-06-14 06:24:23 +0600

    backup

--- main.tf
+++ main.tf
@@ -21,6 +21,14 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_public_ip" "firewall" {
+  name = "firewall-ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet"
   address_space       = ["10.0.0.0/21"]
@@ -33,6 +41,20 @@ resource "azurerm_subnet" "internal" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_firewall" "firewall" {
+  name = "firewall"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku_name = "AZFW_VNet"
+  sku_tier = "Standard"
+
+  ip_configuration {
+    name = "config"
+    subnet_id = azurerm_subnet.internal.id
+    public_ip_address_id = azurerm_public_ip.firewall.id
+  }
 }
 
 resource "azurerm_network_interface" "nic" {
@@ -50,10 +72,14 @@ resource "azurerm_linux_virtual_machine" "web" {
   name                = "web"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_D2s_v3"
-  admin_username      = "azureuser"
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_ssh_key {
+    username = "adminuser"
+    public_key = file("~/.ssh/hasan_rsa.pub")
+  }
   network_interface_ids = [
-    azurerm_virtual_network.vnet.id
+    azurerm_network_interface.nic.id
   ]
 
   os_disk {
@@ -63,8 +89,8 @@ resource "azurerm_linux_virtual_machine" "web" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "minimal-arm64"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 }

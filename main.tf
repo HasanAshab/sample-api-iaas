313ed0e337d0c06bc4494436e3bb6cd60c9ae756
Your Name <you@example.com>
2025-06-14 08:16:35 +0600

    backup

--- main.tf
+++ main.tf
@@ -1,19 +1,26 @@
+variable "subscription" {
+  description = "The subscription id of azure"
+}
+
+variable "location" {
+  description = "Location of all resources"
+}
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = ">= 3.96.0"
     }
   }
   required_version = ">= 1.1.0"
 }
 
+
 provider "azurerm" {
   features {}
-}
 
-variable "location" {
-  description = "Location of all resources"
+  subscription_id = var.subscription
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -48,17 +55,24 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.2.64/26"]
 }
 
 resource "azurerm_subnet" "firewall" {
-  name                 = "firewall-subnet"
+  name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/26"]
 }
 
-resource "azurerm_firewall" "firewall" {
+resource "azurerm_subnet" "management" {
+  name                 = "AzureFirewallManagementSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/26"]
+}
+
+resource "azurerm_firewall" "default" {
   name                = "firewall"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
@@ -73,16 +87,34 @@ resource "azurerm_firewall" "firewall" {
 
   management_ip_configuration {
     name                 = "management"
-    subnet_id            = azurerm_subnet.firewall.id
+    subnet_id            = azurerm_subnet.management.id
     public_ip_address_id = azurerm_public_ip.management.id
   }
 }
 
-resource "azurerm_firewall_policy" "example" {
-  name                = "firewall-policy"
+resource "azurerm_firewall_nat_rule_collection" "default" {
+  name = "firewall-web-rule-collection"
+  azure_firewall_name = azurerm_firewall.default.name
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  priority = 100
+  action = "Dnat"
+
+  rule {
+    name = "web-rule"
+    source_addresses = [ "*" ]
+    destination_ports = [ "4000" ]
+    destination_addresses = [ azurerm_public_ip.firewall.ip_address ]
+    translated_address = azurerm_linux_virtual_machine.web.private_ip_address
+    translated_port = "80"
+    protocols = [ "TCP" ]
+  }
 }
+
+# resource "azurerm_firewall_policy" "example" {
+#   name                = "firewall-policy"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+# }
 
 resource "azurerm_network_interface" "nic" {
   name                = "nic"
@@ -120,4 +152,10 @@ resource "azurerm_linux_virtual_machine" "web" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+}
+
+
+output "firewall_url" {
+  description = "Public URL to access the web server through Azure Firewall"
+  value       = format("http://%s:%s", azurerm_public_ip.firewall.ip_address, "4000")
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,2 @@
 location = "Central India"
+subscription = "fc4b0a21-cf38-4354-9ee7-f3532dd03c57"
